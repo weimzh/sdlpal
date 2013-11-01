45c2c2bdd7a5a0de33eb2bfcbb63213a326513b1
Wei Mingzhi <weimingzhi@gmail.com>
2013-11-02 00:04:28 +0800

    OSX Mavericks compatibility fix.

--- SDLMain.m
+++ SDLMain.m
@@ -373,6 +373,20 @@ int main (int argc, char **argv)
         gFinderLaunch = NO;
     }
 
+    // set current working directory to containing path
+    // required for OSX Mavericks
+    NSBundle *main_bundle = [NSBundle mainBundle];
+    NSString *bundle_path = [main_bundle bundlePath];
+    NSString *to_dir = [bundle_path stringByAppendingString:@"/../"];
+
+    NSFileManager *fm = [NSFileManager defaultManager];
+    [fm changeCurrentDirectoryPath: to_dir];
+
+    [fm release];
+    [to_dir release];
+    [bundle_path release];
+    [main_bundle release];
+
 #if SDL_USE_NIB_FILE
     [SDLApplication poseAsClass:[NSApplication class]];
     NSApplicationMain (argc, argv);
