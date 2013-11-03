f22dc126d2c03e96f650ba44602d7465fd98eb0b
Wei Mingzhi <whistler@Weis-MacBook-Pro.local>
2013-11-03 18:47:10 +0800

    Hackfix for Quit menu in MacOSX

--- SDLMain.m
+++ SDLMain.m
@@ -67,14 +67,15 @@ static NSString *getApplicationName(void)
 @interface SDLApplication : NSApplication
 @end
 
+void PAL_Shutdown();
+
 @implementation SDLApplication
 /* Invoked from the Quit menu item */
 - (void)terminate:(id)sender
 {
     /* Post a SDL_QUIT event */
-    SDL_Event event;
-    event.type = SDL_QUIT;
-    SDL_PushEvent(&event);
+    PAL_Shutdown();
+    exit(0);
 }
 @end
 
